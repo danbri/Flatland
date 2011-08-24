d45e43827e541be6980f58357c84c783bb05c0bc
Dan Brickley <danbri@danbri.org>
2011-08-24 22:25:22 +0200

    ok

--- fakes/foaftest.m
+++ fakes/foaftest.m
@@ -1,18 +1,33 @@
+
+rdfweb;
+
 # http://en.wikipedia.org/wiki/Laplacian_matrix
 # 'Adjacency matrix from early FOAF crawl, pruned of links, 0.7 probability of link drop when each end has 2+ links. Re-ordered using 2nd smallest eigenvector of the graph laplacian.'
 
+# todo: install ghostscript (on osx on this version of Octave, at least; otherwise jpeg writer fails)
+
+fid = fopen('tmp/_report.html','w'); # or 'a' appends
 
 warning("off"); # :( re 'ft_render: skipping missing glyph for character ` '
 more off;
 
 # draw_dot(R1, N1);
+  fprintf(fid, ["<h2>Link Graph</h2>\n"] );
 
-for (K = 1:4);
+for (K = 1:10);
+
+  figure();
+  # first figure
+  #subplot(221)
   disp(sprintf("Run no. %d :",K));
   R2 = prune(R1,0.35);
-
-  draw_dot(R2, N1);
+  draw_dot(R2, cellstr(N1));
   title(sprintf("Reduced rdfweb.org fig %d(a)", K));  # draw matrix chart
+  #subplot(222)
+  fig_no = gcf;
+  fn=sprintf("tmp/_foaf_gviz_%i.jpg",fig_no);
+  saveas(fig_no, fn , 'jpg' );
+  fprintf(fid, [ sprintf("<img width='600' src='../%s' />\n",fn)  ] );
 
   figure();
   L = laplacian(R2);
@@ -28,16 +43,39 @@ for (K = 1:4);
     xlabel('people');
     ylabel('people');
   end
+  fig_no = gcf;
+  fn=sprintf("tmp/_foaf_adjgrid_%i.jpg",fig_no);
+  saveas(fig_no, fn , 'jpg' );
+  fprintf(fid, [ sprintf("<img width='600' src='../%s' />\n",fn)  ] );
   
-  figure();
-  plot(sort(V(:,2)), '.-');
-  title(sprintf("Reduced rdfweb.org fig %d(c) - 2nd smallest eigenvector, sorted values", K));  # draw matrix chart
-  
-end
 
+
+
+  #subplot(223)
+  figure();
+  [foo,neworder] = sort(V(:,2));
+#  plot(sort(V(:,2)), '.-');
+  plot([foo],'.-');
+  title(sprintf("Reduced rdfweb.org fig %d(c) - 2nd smallest eigenvector, sorted values", K));  # draw matrix chart
+  hold on;
+  for J=1:length(N1)
+    NAME = N1(p(J),:);
+#    disp(NAME)
+#    text(1,J,NAME);
+    text( J,foo(J),NAME, 'Rotation', -90);
+  end
+  fig_no = gcf;
+  fn=sprintf("tmp/_foaf_spectral_%i.jpg",fig_no);
+  saveas(fig_no, fn , 'jpg' );
+  fprintf(fid, [ sprintf("<img width='600' src='../%s' />\n",fn)  ] );
+  
+  fprintf(fid, [ sprintf("<hr/>\n\n")] );
+
+end
 
 
 
   # hold on;
   # spy(R2(p,p));
   # hold on;
+fclose(fid);
